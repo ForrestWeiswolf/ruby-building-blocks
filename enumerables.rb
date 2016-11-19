e107a21b6b5de9cf0097dfd44d2f223c56dfe099
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2016-11-19 10:00:39 -0500

    created my_count

--- enumerables.rb
+++ enumerables.rb
@@ -31,9 +31,39 @@ module Enumerable
     return result
   end
 
+  def my_any?
+    result = false
+    if block_given?
+      self.my_each {|i| result = true if yield(i)}
+    else
+      self.my_each{|i| result = true if i}
+    end
+    return result
+  end
+
+  def my_none?
+    !(self.my_any?)
+  end
+
+  def my_count(obj=nil)
+    result = 0
+    if !(obj == nil)
+      self.my_each do |i|
+        result +=1 if i == obj
+      end
+    elsif block_given?
+      self.my_each do |i|
+        result +=1 if yield(i)
+      end
+    else
+      result = self.length
+    end
+    return result
+  end
+
 end
 
-puts %w[ant bear cat].my_all? { |word| word.length >= 3 }
-puts %w[ant bear cat].my_all? { |word| word.length >= 4 }
-puts [nil, true, 99].my_all?
-puts [true, 99].my_all?
+ary = [1, 2, 4, 2]
+puts ary.my_count
+puts ary.my_count(2)
+puts ary.my_count{ |x| x%2==0 }
