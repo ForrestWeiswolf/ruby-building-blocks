c048e04cf6621ceeae4aeb0608a3a747ee3c8c37
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-03-15 10:59:59 -0400

    begun creating tests for #caesar

--- spec/lib/caesar_spec.rb
+++ spec/lib/caesar_spec.rb
@@ -7,8 +7,18 @@ describe "#caesar" do
 		end
 	end
 	context "when given a shift of 2" do
+		it "enciphers 'aBc' as 'cDe'" do
+			expect(caesar('aBc', 2)).to eql('cDe')
+		end
+
 		it "enciphers 'zZ' as 'bB'" do
 			expect(caesar('zZ', 2)).to eql('bB')
+		end
+	end
+
+	context "when there aren't letters in the plaintext" do
+		it "returns it unchanged" do
+			expect(caesar('!! ?? 55', 12)).to eql('!! ?? 55')
 		end
 	end
 end
