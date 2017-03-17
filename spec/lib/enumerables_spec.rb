a0d0d44c83c9f461edacd6a9b117b42b1e4a129f
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-03-17 10:03:26 -0400

    wrote tests for Enumerable.my_all?

--- spec/lib/enumerables_spec.rb
+++ spec/lib/enumerables_spec.rb
@@ -33,4 +33,55 @@ describe Enumerable do
 			end
 		end
 	end
+
+	describe ".my_all?" do
+		context "when called on an empty list" do #is there some way to make this DRYer?
+			it "returns true" do
+				expect([].my_all?{|i| true}).to eql(true)
+			end
+		end
+
+		context "when passed a block that only returns false" do
+			it "returns false" do
+				expect(an_enumerable.my_all?{|i| false}).to eql(false)
+			end
+		end
+		context "when passed a block that only returns true" do
+			it "returns true" do
+				expect(an_enumerable.my_all?{|i| true}).to eql(true)
+			end
+		end
+
+		context "when block is true for every member of self" do
+			it "returns true" do
+				expect(an_enumerable.my_all?{|i| i<100}).to eql(true)
+			end
+		end
+
+		context "when block is false for some member of self" do
+			it "returns false" do
+				expect(an_enumerable.my_all?{|i| i<5}).to eql(false)
+			end
+		end
+
+		context "when no block is given" do
+			context "when called by a list of truthy values" do
+				it "returns true" do
+					expect(an_enumerable.my_all?).to eql(true)
+				end
+			end
+			context "when called by a list containing false or nil" do
+				it "returns false" do
+					expect([true, false].my_all?).to eql(false)
+					expect([1, 1, nil].my_all?).to eql(false)
+				end
+			end
+		end
+
+		context "when not called by an enumerable" do
+			it "raises an error" do
+				expect { 1.my_all?{|i| true} }.to raise_error(NoMethodError)
+			end
+		end
+	end
 end
